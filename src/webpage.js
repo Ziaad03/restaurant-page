f2a61ac0a3616d48e405a2cb180ff24de76973f0
Ziaad03 <ziadd.essam03@gmail.com>
2024-05-10 02:56:38 +0300

    complete the home page layout

--- src/webpage.js
+++ src/webpage.js
@@ -1,9 +1,10 @@
  import './style/home.css'
  import logoIcon from './images/logo.jpeg' ;
- 
+ import contentImage from './images/sryan-food-img.jpg'
  export default function homeLoad(){
+    
 
-    // add logo to nav
+    // add to nav
     const logo = new Image()
     logo.src = logoIcon;
 
@@ -12,13 +13,24 @@
 
 
 
+    // get the elements by id
     const content = document.getElementById('content')
+    let textContent = document.getElementById('text-content')
 
-    let textContent = document.createElement('div')
+    // get content  food image
+    const contentImg = new Image()
+    contentImg.src = contentImage;
+    
+    // add to content
+    const imageContent = document.getElementById('image-content')
+    imageContent.append(contentImg)
+
+
+    
     let title = document.createElement('h1')
     title.textContent = "عيون الشام";
     let paraText = document.createElement('p')
-    paraText.textContent = "For delicious Siryan food";
+    paraText.textContent = "For delicious Siryan meals";
     let discoverBtn = document.createElement('button');
     discoverBtn.textContent = "Discover Menu";
     textContent.append(title)
@@ -27,5 +39,6 @@
     
 
     content.append(textContent)
+    content.append(imageContent)
 
 }
